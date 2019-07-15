6638ee6f1525eb21f33949c42b8021ca9fd3c32f
Carlos Alfaro <carlosalfaro@finblok.co>
2019-07-15 14:52:06 -0600

    Created fragments for notes and Tasks Created Task adapter.

---
+++ app/src/main/java/com/example/notes/navigation/NavigationActivity.kt
@@ -0,0 +1,46 @@
+package com.example.notes.navigation
+
+import android.os.Bundle
+import android.view.MenuItem
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.example.notes.R
+import com.example.notes.note.NotesListFragment
+import com.example.notes.tasks.TasksListFragment
+import kotlinx.android.synthetic.main.activity_navigation.*
+
+class NavigationActivity : AppCompatActivity() {
+
+
+    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item: MenuItem ->
+        when (item.itemId) {
+            R.id.navigation_tasks -> {
+                replaceFragment(TasksListFragment.newInstance())
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_notes -> {
+                replaceFragment(NotesListFragment.newInstance())
+                return@OnNavigationItemSelectedListener true
+            }
+        }
+        false
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_navigation)
+        replaceFragment(TasksListFragment.newInstance())
+        navigationView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.fragmentHolder, fragment)
+            .commit()
+
+    }
+
+}
---
+++ app/src/main/java/com/example/notes/note/NotesListFragment.kt
@@ -0,0 +1,29 @@
+package com.example.notes.note
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.example.notes.R
+
+
+class NotesListFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_notes_list, container, false)
+    }
+
+    companion object {
+        fun newInstance() = NotesListFragment()
+    }
+}
