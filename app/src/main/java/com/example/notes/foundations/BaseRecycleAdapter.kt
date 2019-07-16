7347b1d395949dd27f2bccb4cfac555b854d2ac9
Carlos Alfaro <carlosalfaro@finblok.co>
2019-07-16 14:02:46 -0600

    Added nested view for notes. Managed the constatnts and list size from the base recyvler adapter

--- app/src/main/java/com/example/notes/foundations/BaseRecycleAdapter.kt
+++ app/src/main/java/com/example/notes/foundations/BaseRecycleAdapter.kt
@@ -2,20 +2,38 @@ package com.example.notes.foundations
 
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
+import com.example.notes.R
+import com.example.notes.tasks.TaskAdapter
+import kotlinx.android.synthetic.main.view_add_button.view.*
 
 abstract class BaseRecycleAdapter<T>(
     protected val masterList: MutableList<T> = mutableListOf()
 ) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
-    override fun getItemCount(): Int = masterList.size
+    override fun getItemViewType(position: Int): Int = if (position == 0) {
+        TYPE_ADD_BUTTON
+    } else {
+        TYPE_INFO
+    }
+
+    override fun getItemCount(): Int = masterList.size + 1
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        (holder as BaseViewHolder<T>).onBind(masterList[position])
+        if (holder is AddButtonViewHolder) {
+            holder.onBind(Unit)
+        } else {
+            (holder as BaseViewHolder<T>).onBind(masterList[position - 1])
+        }
     }
 
     abstract class BaseViewHolder<E>(val view: View) : RecyclerView.ViewHolder(view) {
         abstract fun onBind(data: E)
     }
 
+    abstract class AddButtonViewHolder(view: View) : BaseViewHolder<Unit>(view)
 
+    companion object {
+        const val TYPE_ADD_BUTTON = 0
+        const val TYPE_INFO = 1
+    }
 }
