5bc6ce544c51c168e4e79dfdd466e042c96cfad6
Carlos Alfaro <carlosalfaro@finblok.co>
2019-07-16 10:50:01 -0600

    created custom view in java for the todos as a separate class for the task adapter not to change to do views but only manage tasks.

--- app/src/main/java/com/example/notes/tasks/TaskAdapter.kt
+++ app/src/main/java/com/example/notes/tasks/TaskAdapter.kt
@@ -1,6 +1,6 @@
 package com.example.notes.tasks
 
-import android.graphics.Paint
+
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -8,9 +8,9 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.notes.R
 import com.example.notes.foundations.BaseRecycleAdapter
 import com.example.notes.models.Task
-import kotlinx.android.synthetic.main.item_note.view.*
+import com.example.notes.views.TodoView
 import kotlinx.android.synthetic.main.item_task.view.*
-import kotlinx.android.synthetic.main.view_todo.view.*
+
 
 class TaskAdapter(
     taskList: MutableList<Task> = mutableListOf()
@@ -26,19 +26,8 @@ class TaskAdapter(
             view.titleView.text = data.title
 
             data.todos.forEach {todo ->
-                val todoView = LayoutInflater.from(view.context).inflate(R.layout.view_todo, view.todoContainer, false).apply {
-                    todoDescriptionView.text = todo.description
-                    completeCheckBox.isChecked = todo.isComplete
-                    if (todo.isComplete) {
-                        todoDescriptionView.paintFlags = todoDescriptionView.paintFlags or Paint.STRIKE_THRU_TEXT_FLAG
-                    }
-                    completeCheckBox.setOnCheckedChangeListener { button, isChecked ->
-                        if (isChecked) {
-                            todoDescriptionView.paintFlags = todoDescriptionView.paintFlags or Paint.STRIKE_THRU_TEXT_FLAG
-                        } else {
-                            todoDescriptionView.paintFlags = todoDescriptionView.paintFlags and Paint.STRIKE_THRU_TEXT_FLAG.inv()
-                        }
-                    }
+                val todoView = (LayoutInflater.from(view.context).inflate(R.layout.view_todo, view.todoContainer, false) as TodoView).apply {
+                    initView(todo)
                 }
                 view.todoContainer.addView(todoView)
             }
